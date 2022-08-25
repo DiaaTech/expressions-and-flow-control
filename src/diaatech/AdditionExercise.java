ef99dda81cd1e59a54d3004f7925295125fb1a06
your-name-herekhaled anwar <your-email-herekhaledabdu01@gmail.com>
2022-08-24 21:50:18 -0500

    added single addition exercise

---
+++ src/diaatech/AdditionExercise.java
@@ -0,0 +1,13 @@
+package diaatech;
+
+public class AdditionExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int result = 0;
+		  for(int i=1; i<=10; i++) {
+			 result= result + i;
+		   }
+	      System.out.print(result);
+	      }
+      }
